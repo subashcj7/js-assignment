df0d3d07c356285a585547971c68c8f16ef52b69
subashcj7 <52062605+subashcj7@users.noreply.github.com>
2020-07-15 22:25:10 +0530

    Assignment-3

---
+++ Day4q.js
@@ -0,0 +1,29 @@
+//1)Js code which takes input form the user and logs into console
+
+let val=window.prompt("Enter your choice");
+alert("your choice is"+ val)
+console.log(val)
+
+// 2)Methods of string and array
+// ->push() and pop()
+// ->shift() and unshift()
+// ->changing elements 
+// ->length
+// ->Deleting ann element
+// ->splicing an Array
+// ->merging Array
+// ->concat()
+// ->slice()
+// ->toString()
+// ->parseInt()
+// ->parseFloat()
+
+
+//3)
+let val=23;
+  if(val > 21){
+     console.log("Can drink")
+  }
+  else{
+     console.log("Cannot drink")
+  }
